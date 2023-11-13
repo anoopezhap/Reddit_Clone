97908d33f1c90cf766fa964a9d98d130196e776c
anoopezhap <anoopezhap@gmail.com>
2023-11-13 16:09:41 -0500

    nov 13 from work

--- src/features/Posts/CreatePost.jsx
+++ src/features/Posts/CreatePost.jsx
@@ -1,5 +1,50 @@
+import { useForm } from "react-hook-form";
+
 function CreatePost() {
-  return <div>create Post</div>;
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <div className="flex justify-center items-center w-screen h-screen bg-white">
+        <div className="container mx-auto my-4 px-4 lg:px-20">
+          <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
+            <div className="flex">
+              <h1 className="font-bold uppercase text-5xl">Create Post</h1>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+                <input
+                  className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="Post Title"
+                  {...register("title")}
+                />
+              </div>
+              <div className="my-4">
+                <textarea
+                  placeholder="Post Description*"
+                  className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  {...register("description")}
+                ></textarea>
+              </div>
+              <div className="my-2 w-1/2 lg:w-1/4">
+                <button
+                  className="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
+                focus:outline-none focus:shadow-outline"
+                >
+                  Create Post
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreatePost;
--- src/features/Posts/PostRow.jsx
+++ src/features/Posts/PostRow.jsx
@@ -1,17 +1,64 @@
 function PostRow({ post }) {
-  const { id, title, description, user, likes, comments } = post;
+  //const { id, title, description, user, likes, comments } = post;
 
-  console.log(post);
+  //console.log(post);
 
   return (
-    <div>
-      <div>
-        <h2> User Name :{user.userName}</h2>
-        <h2>Title : {title}</h2>
-        <h2>Description :{description}</h2>
-        <h2>Likes {likes.length}</h2>
-        <h2>Comments {comments.length}</h2>
-        <br />
+    // <div>
+    //   {/* <div>
+    //     <h2> User Name :{user.userName}</h2>
+    //     <h2>Title : {title}</h2>
+    //     <h2>Description :{description}</h2>
+    //     <h2>Likes {likes.length}</h2>
+    //     <h2>Comments {comments.length}</h2>
+    //     <br />
+    //   </div> */}
+    //   Anoo
+    // </div>
+
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="md:flex">
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            anoopezhap
+          </div>
+          <a
+            href="#"
+            className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+          >
+            This is my first ppost
+          </a>
+          <p className="mt-2 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            maiores! Totam hic in ipsum. Amet quae reiciendis nemo dicta fugit
+            enim nostrum dignissimos, aliquam inventore quidem corrupti!
+            Obcaecati, maxime et?
+          </p>
+          <div className="mt-4 flex items-center">
+            <div className="flex items-center text-gray-600">
+              <svg
+                className="h-5 w-5 mr-1"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              ></svg>
+              <span className="mr-2">Likes</span>
+              <span className="font-semibold">42</span>
+            </div>
+            <div className="flex items-center ml-6 text-gray-600">
+              <svg
+                className="h-5 w-5 mr-1"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              ></svg>
+              <span className="mr-2">Comments</span>
+              <span className="font-semibold">15</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
